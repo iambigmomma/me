cf2951c07ade05e2f46e7f34c7aa384bb9058671
Jeff Fan <iambigmomma@gmail.com>
2022-05-11 08:19:27 +0100

    Initial commit

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,16 +59,16 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Jeff Fan.</h2>;
+  const three = <h3 className="big-heading">I connect the dots.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a solution engineer specializing in advocating cloud computing concept in this great
+        computer science era. Currently, I’m focused on helping customers to build products with
+        digital confidence at{' '}
+        <a href="https://saucelabs.com/" target="_blank" rel="noreferrer">
+          Sauce Labs.
         </a>
         .
       </p>
